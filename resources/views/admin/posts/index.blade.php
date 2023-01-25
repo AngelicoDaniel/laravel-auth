0e0d0a8c0e194c843dbfbf7e94d7896b605d0e68
Daniel <angelicodaniel93@gmail.com>
2023-01-25 14:52:28 +0100

    faker

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.dashboard')
+@section('content')
+    <h1>i post</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function(){
+        Route::get('/', 'HomeController@index')->name('index');
+        //crud controller
+        Route::resource('/posts', PostController::class);
+    });
+
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('{any?}', function(){
+    return view('guest.home');
+})->where("any", ".*");
